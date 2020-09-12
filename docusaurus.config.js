21cb6553ea27545400f53a29bd3c79b2416cec36
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2020-09-12 15:02:34 +0200

    initialized template

---
+++ docusaurus.config.js
@@ -0,0 +1,87 @@
+module.exports = {
+  title: 'VSpaceCode',
+  tagline: 'Spacemacs like keybindings for Visual Studio Code',
+  url: 'https://vspacecode.github.io/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'VSpaceCode', // Usually your GitHub org/user name.
+  projectName: 'vspacecode.github.io', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'VSpaceCode',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/VSpaceCode/vspacecode.github.io',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/VSpaceCode/VSpaceCode',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/VSpaceCode/vspacecode.github.io/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/VSpaceCode/vspacecode.github.io/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
