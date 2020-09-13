87a39522cf4ee3b65df8543bb369e9120d2fe6e3
Steven Guh <stevenguh@gmail.com>
2020-09-13 13:53:10 -0700

    Update index's content

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,7 +62,7 @@ module.exports = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} VSpaceCode. Built with Docusaurus.`,
     },
   },
   presets: [
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,38 +8,36 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: 'Easy to Use',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    title: 'Mnemonic Bindings',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        VSpaceCode provides key bindings that are organized by mnemonic
+        key like <code>f</code> for file and <code>p</code> for project
+        and similar to spacemacs.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    title: 'Batteries Included',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        VSpaceCode aims to provide the best out-of-the-box experience and
+        bundles all the extensions needed out-of the box.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
+    title: 'Customizable',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        The key binding and its menu are fully customizable. VSpaceCode aims
+        to provide an escape hatch whenever possible to allow customization.
       </>
     ),
   },
 ];
 
-function Feature({imageUrl, title, description}) {
+function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
     <div className={clsx('col col--4', styles.feature)}>
@@ -56,7 +54,7 @@ function Feature({imageUrl, title, description}) {
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
